962c0a40ba523780fcb7e7562feeec13ffaa01b3
MiraViktoria <Seraphine1226@gmail.com>
2019-12-09 19:12:21 -0600

    sending emails

---
+++ src/test/java/Gmail/Credentials.java
@@ -0,0 +1,16 @@
+package Gmail;
+
+public class Credentials {
+
+    private String userName = "Cybertek.Batch@gmail.com";
+    private String passWord = "Batch12BestBatch";
+
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassWord(){
+        return passWord;
+    }
+
+
+}
